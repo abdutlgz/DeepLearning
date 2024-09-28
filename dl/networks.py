bd07da6207f1eb8e879890d0f6e498a127920066
abdutlgz <65399051+abdutlgz@users.noreply.github.com>
2024-09-28 00:11:01 -0400

    Update networks.py

--- dl/networks.py
+++ dl/networks.py
@@ -1,6 +1,37 @@
 import torch
 import torch.nn as nn 
 
+class LeNet5(nn.Module):
+    def __init__(self):
+        super(LeNet5, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, padding=2, padding_mode='reflect')
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
+        
+            self.fc1 = nn.Linear(in_features=16*5*5, out_features=120)  # 5x5 is the size after two convs and poolings
+        self.fc2 = nn.Linear(in_features=120, out_features=84)
+        self.fc3 = nn.Linear(in_features=84, out_features=10)
+
+    def forward(self, x):
+        #conv+relu+maxpool
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, kernel_size=2, stride=2)
+        
+        #conv+relu+maxpool
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, kernel_size=2, stride=2)
+        
+        #flatten tensor
+        x = x.view(-1, 16 * 5 * 5) 
+        
+        # Fully connected layers + ReLU
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        
+        #output
+        x = self.fc3(x)
+        return x
+    
 class Dense(nn.Module):
     """
     Fully Connected feed forward network.
